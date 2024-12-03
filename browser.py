40d68c16a2fffabee22e9f8e390b342e4d3d85b9
Vlad Cojocaru <vladvcojocaru@gmail.com>
2024-12-03 09:34:44 +0200

    finish file read (ex 1.2)

--- browser.py
+++ browser.py
@@ -5,69 +5,82 @@ class URL:
     def __init__(self, url) -> None:
         self.scheme, url = url.split("://", 1)
 
-        assert self.scheme in ["http", "https"]
+        assert self.scheme in ["http", "https", "file"]
 
         if self.scheme == "http":
             self.port = 80
         elif self.scheme == "https":
             self.port = 443
+        elif self.scheme == "file":
+            self.port = None
 
 
-        if "/" not in url:
-            url += "/"
-        self.host, url = url.split("/", 1)
-        self.path = "/" + url
+        if self.scheme in ["http", "https"]:
+            if "/" not in url:
+                url += "/"
+                self.host, url = url.split("/", 1)
+                self.path = "/" + url
 
-        if ":" in self.host:
-            self.host, port = self.host.split(":", 1)
-            self.port = int(port)
+            if ":" in self.host:
+                self.host, port = self.host.split(":", 1)
+                self.port = int(port)
+
+        elif self.scheme == "file":
+            self.path = url
 
     def request(self):
-        s = socket.socket(
-            family=socket.AF_INET,
-            type=socket.SOCK_STREAM,
-            proto=socket.IPPROTO_TCP
-        )
-        s.connect((self.host, self.port))
-        if self.scheme == "https":
-            ctx = ssl.create_default_context()
-            s = ctx.wrap_socket(s, server_hostname=self.host)
+        if self.scheme in ["http", "https"]:
+            s = socket.socket(
+                family=socket.AF_INET,
+                type=socket.SOCK_STREAM,
+                proto=socket.IPPROTO_TCP
+            )
+            s.connect((self.host, self.port))
+            if self.scheme == "https":
+                ctx = ssl.create_default_context()
+                s = ctx.wrap_socket(s, server_hostname=self.host)
 
-        headers = {
-            "Host": self.host,
-            "User-Agent": "my-custom-browser",
-            "Connection": "close"
-        }
-        request = f"GET {format(self.path)} HTTP/1.1\r\n"
-        for key, value in headers:
-            request += f"{key}: {value}\r\n"
-        request += "\r\n"
+            headers = {
+                "Host": self.host,
+                "User-Agent": "my-custom-browser",
+                "Connection": "close"
+            }
+            request = f"GET {format(self.path)} HTTP/1.1\r\n"
+            for key, value in headers.items():
+                request += f"{key}: {value}\r\n"
 
-        s.send(request.encode("utf8"))
+            request += "\r\n"
 
-        response = s.makefile('r', encoding='utf8', newline='\r\n')
+            s.send(request.encode("utf8"))
 
-        statusline = response.readline()
-        version, status, explanation = statusline.split(" ", 2)
+            response = s.makefile('r', encoding='utf8', newline='\r\n')
 
-        response_headers = {}
-        while True:
-            line = response.readline()
-            if line == "\r\n":
-                break;
+            statusline = response.readline()
+            version, status, explanation = statusline.split(" ", 2)
 
-            header, value = line.split(":", 1)
-            response_headers[header.casefold()] = value.strip()
+            response_headers = {}
+            while True:
+                line = response.readline()
+                if line == "\r\n":
+                    break;
 
-        assert "transfer-encoding" not in response_headers
-        assert "content-encoding" not in response_headers
+                header, value = line.split(":", 1)
+                response_headers[header.casefold()] = value.strip()
 
-        content = response.read()
-        s.close()
+            assert "transfer-encoding" not in response_headers
+            assert "content-encoding" not in response_headers
 
-        return content
+            content = response.read()
+            s.close()
 
-def show(body: str):
+            return content
+
+        elif self.scheme == "file":
+            with open(self.path, "r") as f:
+                return f.read()
+
+
+def show(body):
     in_tag = False
     for c in body:
         if c == "<":
@@ -84,4 +97,7 @@ def load(url: URL):
 
 if __name__ == "__main__":
     import sys
-    load(URL(sys.argv[1]))
+    if len(sys.argv) == 1:
+        load(URL("file:///home/vlad/code/browser-py/browser.py"))
+    else:
+        load(URL(sys.argv[1]))
